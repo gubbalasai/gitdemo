b3879d73e1f2d49f25e7debddd167612564535df
gubbalasai <saimanikantagubbala0706@gmail.com>
2022-07-13 22:12:27 +0530

    first commit

---
+++ src/main/java/com/digit/pakodi/convertintogit/App.java
@@ -0,0 +1,13 @@
+package com.digit.pakodi.convertintogit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
